157a4f60e074d09a57dbf86de19011d89971fb01
Jim Montour <jimmontour@gmail.com>
2020-09-23 14:26:08 -0400

    Added event listeners

---
+++ script.js
@@ -0,0 +1,28 @@
+let myLibrary = [];
+
+function Book() {
+    // the constructor
+};
+
+function addBookToLibrary() {
+    // do stuff here
+}
+
+// make a functional form with event listeners and query selectors
+let newTitle = document.querySelector('#title').value;
+let newAuthor = document.querySelector('#author').value;
+let newPages = document.querySelector('#pages').value;
+
+const submit = document.querySelector('#submit-button')
+
+// when submit is clicked
+submit.addEventListener('click', function(e) {
+
+});
+
+
+// the title, author and pages field values are recorded
+// these values are put together as a new object from the object constructor
+// this new object is then added to the myLibrary array
+// the myLibrary array is shown on the page with the new book
+
